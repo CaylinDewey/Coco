c0bdcd1d47b41353fe35e99c09fd19f6a32ef44d
Caylin Dewey <caylindewey42@gmail.com>
2024-02-07 10:16:24 +0000

    Add landing page view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
 # Create your views here.
-def my_blog(request):
-    return HttpResponse("Hello, Blog!")
+# def my_blog(request):
+#    return HttpResponse("Hello, Blog!")
+# Create your views here.
+class PostList(generic.ListView):
+    model = Post
