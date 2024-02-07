3f409bcf24d088cf0d60ff7b4c9cc03bf3b29723
Caylin Dewey <caylindewey42@gmail.com>
2024-02-07 12:25:28 +0000

    Updated views

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 # from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import generic
 from .models import Post
+
 
 # Create your views here.
 # def my_blog(request):
 #    return HttpResponse("Hello, Blog!")
 # Create your views here.
 class PostList(generic.ListView):
-#    model = Post
+        # model = Post
     queryset = Post.objects.all()
     template_name = "post_list.html"
