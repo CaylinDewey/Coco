8783be5b36a2d315c3428152596582ecdd2dc099
Caylin Dewey <caylindewey42@gmail.com>
2024-02-08 10:33:08 +0000

    update create field to correct error

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,10 @@
-# from django.shortcuts import render
-# from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import generic
 from .models import Post
 
+# Create your views here.
 
-# Create your views here.
-# def my_blog(request):
-#    return HttpResponse("Hello, Blog!")
-# Create your views here.
+
 class PostList(generic.ListView):
-        # model = Post
+    model = Post
     queryset = Post.objects.filter(status=1)
-    template_name = "post_list.html"
